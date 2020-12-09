b0db0c1c4ee835551da56728b57a77eb93b30e59
Joshua Wakhata <hatah01@gmail.com>
2020-12-09 21:13:18 +0300

    End of section 1

---
+++ 2020/starecon/frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import reportWebVitals from "./reportWebVitals";
+
+const rootEl = document.getElementById("root");
+
+function render() {
+  ReactDOM.render(<App />, rootEl);
+}
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App", function () {
+    setTimeout(render);
+  });
+}
+
+render();
+
+reportWebVitals();
